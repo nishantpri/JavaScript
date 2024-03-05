edc87d8fdf21784b0f033b5d86a1b8865a255beb
nishantpri <101243226+nishantpri@users.noreply.github.com>
2024-03-06 01:54:36 +0530

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,15 @@
+//operations in javascript
+
+let val=3;
+let negValue=-val // This  will give negative value of  the variable val
+// console.log(negValue);
+// Tricky part
+ 
+console.log("1"+2); // This will give output as 12;
+console.log(1+"2");// This will give output as 12
+console.log("1"+2+2);// This will give output as 122
+console.log(1+2+"2");// This will give output as 32
+
+// operator on mdn follow mdn for documentation reading
+
+
