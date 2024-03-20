327a595ed7a0537e58622b3c53ef4bd9e416d799
nishantpri <101243226+nishantpri@users.noreply.github.com>
2024-03-20 15:24:00 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -20,19 +20,51 @@
 //  const result=addTwoNum(2,3);
 //  console.log(result);  // this will print 5 first then it will print undefined  to ovecome this we will do the followinf
 
-function addTwoNum(num1,num2){
-    return num1+num2;
- }
- const result=addTwoNum(3,5);
- console.log(result);
+// function addTwoNum(num1,num2){
+//     return num1+num2;
+//  }
+//  const result=addTwoNum(3,5);
+//  console.log(result);
 
- function LogInUser(Username){
-    if(Username===undefined || Username ===""){
-        console.log("Please enter valid UserName")
-        return;
-    }
-    return`${Username} just logged in`
+//  function LogInUser(Username){
+//     if(Username===undefined || Username ===""){
+//         console.log("Please enter valid UserName")
+//         return;
+//     }
+//     return`${Username} just logged in`
     
- }
- console.log(LogInUser("Nishant")) // if we will not pass any argument this  will automatically pass undefined as argument
+//  }
+//  console.log(LogInUser("Nishant")) // if we will not pass any argument this  will automatically pass undefined as argument
+
+
+//  function calCartPrice(...num1){   
+
+//     return num1;
+//  }
+//  console.log(calCartPrice(2,3,4,5,5));
+//  //...is also called rest operator depend upon use  .. this will convert this to an as which we can use futher to sum and all  as required 
  
+
+ const user={
+   userName:"Nishant",
+   price:199
+}
+function handleObject(anyobject){// we need to check type safety of object //like chech object is of same type or not
+
+   console.log(`usernamr is ${anyobject.userName} and price is  ${anyobject.price}`);
+
+}
+handleObject(user);
+
+// we can also pass
+handleObject({
+   userName:"Pritam",
+   price:200
+})
+// we can also pass array
+const myArr=[2,3,4,56];
+
+function retunSecondVal(getArray){
+   return getArray[1];
+}
+console.log(retunSecondVal(myArr));
